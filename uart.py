474270f0b9da0eff81800bcf52a84660a920b1c7
Nam Hoang <89258365+dhnamn@users.noreply.github.com>
2024-04-11 20:53:45 +0700

    Fix time axis

--- uart.py
+++ uart.py
@@ -1,4 +1,3 @@
-from turtle import delay
 import serial
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -19,20 +18,31 @@ xdata, ydata = [], []
 line, = ax.plot([], [], lw=2)
 
 # Khởi tạo giới hạn ban đầu cho trục x
-ax.set_xlim(0, 20)
+ax.set_xlim(0, 300)
 ax.set_ylim(0, 3.3)
+
+def getData():
+    str = ""
+    count = 0
+    while count < 40:
+        binar = ser.read()
+        if binar == b'\x00':
+            continue
+        if binar == b'u':
+            break
+        str += binar.decode('utf-8')
+    return int(str)
 
 def update(frame):
     # Đọc dữ liệu từ UART
-    data = ser.readline().decode('utf-8').rstrip() 
+    data = getData()
     # data(0 -> 4096) tương ứng điện áp (0 -> 3.3V)
     y = float(data) / 4096 * 3.3  
     xdata.append(frame)
     ydata.append(y)
     line.set_data(xdata, ydata)
     # Cập nhật giới hạn trục x để cuộn trục
-    ax.set_xlim(frame-20, frame+1)
-    delay(50)
+    ax.set_xlim(frame-300, frame+1)
     return line,
 
 ani = FuncAnimation(fig, update, frames=range(1000), blit=False, interval=50)
